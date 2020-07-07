4ff1d672689ed9df95ee9ec2c027cabb573d16ea
Sohpronios <frsophronios@sophronios.com>
2020-07-07 03:08:31 -0400

    working library now

--- lua/fu.lua
+++ lua/fu.lua
@@ -1,46 +1,5 @@
 fu = {}
 
-function find(e,xs)
-  if (isempty(xs)) then
-    return false 
-  elseif (e == xs[1]) then
-    return true
-  else
-    return find(e, tail(xs))
-  end
-end
-
-function notuniq(xs)
-  function dubitr(acc, xs)
-    local r 
-    if fu.isempty(xs) then
-      return acc 
-    elseif (find(xs[1],fu.tail(xs)) and not find(xs[1], acc)) then
-      r = xs[1]
-      return dubitr(table.insert(acc, r), fu.tail(xs))
-    else
-      return dubitr(acc, fu.tail(xs))
-    end
-  end
-  return dubitr({}, xs)
-end
-
-function uniq(xs)
-  local duplicates = notuniq(xs)
-  local acc = {}
-  function uniqitr(acc, d, xs)
-    if (isempty(xs)) then
-      return acc
-    elseif (find(xs[1],d)) then
-      return uniqitr(acc, d, tail(xs))
-    else
-      table.insert(acc, xs[1])
-      return uniqitr(acc, d, tail(xs))
-    end
-  end
-  return uniqitr(acc, duplicates, xs)
-end
-
 local function head(xs)
   return xs[1]
 end
@@ -55,6 +14,47 @@ end
 
 local function isempty(s)
   return s == nil or s == '' or #s <= 0
+end
+
+local function find(e,xs)
+  if (isempty(xs)) then
+    return false 
+  elseif (e == xs[1]) then
+    return true
+  else
+    return find(e, tail(xs))
+  end
+end
+
+local function notuniq(xs)
+  function dubitr(acc, xs)
+    local r 
+    if isempty(xs) then
+      return acc 
+    elseif (find(xs[1], tail(xs)) and not find(xs[1], acc)) then
+      r = xs[1]
+      return dubitr(table.insert(acc, r), tail(xs))
+    else
+      return dubitr(acc, tail(xs))
+    end
+  end
+  return dubitr({}, xs)
+end
+
+local function uniq(xs)
+  local duplicates = notuniq(xs)
+  local acc = {}
+  function uniqitr(acc, d, xs)
+    if (isempty(xs)) then
+      return acc
+    elseif (find(xs[1],d)) then
+      return uniqitr(acc, d, tail(xs))
+    else
+      table.insert(acc, xs[1])
+      return uniqitr(acc, d, tail(xs))
+    end
+  end
+  return uniqitr(acc, duplicates, xs)
 end
 
 local function foldl(f, acc, xs)
